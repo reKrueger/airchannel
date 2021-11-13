6c4accf0acd993fc7b3f391ddc4b8cd359f566a5
reKrueger <64320105+reKrueger@users.noreply.github.com>
2021-11-13 09:30:06 +0100

    color change

--- src/infoView/index.js
+++ src/infoView/index.js
@@ -20,21 +20,13 @@ class InfoView extends React.Component{
     componentDidMount(){
     }
 
-    componentDidUpdate(prevProps){
-      
-    }
-
-
-
-  
-  
     render(){
       const address = ' bc1qxra7pp8nf3g7uk6ugs06tkm8fangc4palnmnun'
       return (
         <div className='helpView'>
             <div className='backToAirChannel_div' onClick={()=>this.props.history.push('/')}>back to <img className='air_icon' src={air} alt="Logo"/> AIR channel</div>
             ... non sign in temporary File transfer on web !
-            <div style={{marginTop:'50px'}}>spend Bitcoins:  <div style={{color:'hotpink'}}>{address}</div></div>
+            <div style={{marginTop:'50px'}}>donate Bitcoins:  <div style={{color:'hotpink'}}>{address}</div></div>
         </div>
       )
     }
